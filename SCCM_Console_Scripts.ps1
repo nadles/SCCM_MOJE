7803d170fb1417e6557e76e5c4062cd06cb9f6e1
nadles <mrnadles@gmail.com>
2025-11-19 11:45:41 +0100

    Update SCCM_Console_Scripts.ps1

--- SCCM_Console_Scripts.ps1
+++ SCCM_Console_Scripts.ps1
@@ -1,1 +1,5 @@
-
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000001}" # Hardware Inventory
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000021}" # Machine Policy Assignments Request
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000022}" # Machine Policy Evaluation
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000113}" # Scan by Update Source
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000108}" # Software Updates Assignments Evaluation Cycle
